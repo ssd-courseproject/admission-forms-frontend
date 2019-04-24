56555fbfe33cdfb55888167eb1e37a101100c8d1
Ruslan Farkhutdinov <fr3ddy4@yandex.ru>
2019-04-24 16:44:20 +0300

    Hotfix: live search on tests page fixed

--- src/components/Tests/TestList/index.jsx
+++ src/components/Tests/TestList/index.jsx
@@ -5,64 +5,66 @@ import '../index.less';
 import actions from '../../../redux/actions'
 
 function onSearchTest(search) {
-    const entry = search.target.value;
+  const entry = search.target.value;
+  console.log(entry);
 
-    this.props.dispatch(actions.tests.modifyTestsList(test => test.test_name.toLowerCase().includes(entry.toLowerCase())));
+  function testEntry(test, entry) {
+    return test.test_name.toLowerCase().includes(entry && entry.toLowerCase());
+  }
+
+  const initialList = this.props.testsList.list;
+
+  this.props.dispatch(actions.tests.modifyTestsList(initialList, test => testEntry(test, entry)));
 }
 
 
 class TestList extends Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
+  }
 
-        this.state = {
-            tests: props.testsList.list && props.testsList.list.list || [],
-        }
-    }
+  componentDidMount() {
+    this.props.dispatch(actions.tests.fetchTestsList());
+  }
 
-    componentDidMount() {
-        this.props.dispatch(actions.tests.fetchTestsList());
-    }
+  render() {
+    return (
+      <div className="test-list">
+        <p className="tests-info"><span> Be aware: </span> All tests have a timer that starts when you open the
+          test page. The timer is topped when the Submit button is
+          pressed at the end of the test. If the timer finishes before you the completed data is saved and
+          sent. </p>
+        <p className="test-title">Tests available: </p>
+        <input onChange={onSearchTest.bind(this)} className="search" type="text"
+               placeholder="Search for specific tests..."/>
 
-    render() {
-        console.log(this.props);
-        return (
-            <div className="test-list">
-                <p className="tests-info"><span> Be aware: </span> All tests have a timer that starts when you open the
-                    test page. The timer is topped when the Submit button is
-                    pressed at the end of the test. If the timer finishes before you the completed data is saved and
-                    sent. </p>
-                <p className="test-title">Tests available: </p>
-                <input onChange={onSearchTest.bind(this)} className="search" type="text"
-                       placeholder="Search for specific tests..."/>
-
-                {this.props.testsList.list && this.props.testsList.list.list && this.props.testsList.list.list.map(test => (
-                    <div className="test-entry" key={test.id}>
-                        <div className="test-header">
-                            <Link to={`/test/${test.id}`}>
-                                <p className="name">
-                                    {test.test_name}
-                                </p>
-                            </Link>
-                            <div className="meta">
-                                <p>Time given: <span>{test.max_time}</span></p>
-                            </div>
-                        </div>
-                        <p className="description">
-                            {test.description}
-                        </p>
-                    </div>
-                ))}
+        {this.props.testsList.filteredList && this.props.testsList.filteredList && this.props.testsList.filteredList.map(test => (
+          <div className="test-entry" key={test.id}>
+            <div className="test-header">
+              <Link to={`/test/${test.id}`}>
+                <p className="name">
+                  {test.test_name}
+                </p>
+              </Link>
+              <div className="meta">
+                <p>Time given: <span>{test.max_time}</span></p>
+              </div>
             </div>
-        )
-    }
+            <p className="description">
+              {test.description}
+            </p>
+          </div>
+        ))}
+      </div>
+    )
+  }
 }
 
 function mapStateToProps(state) {
-    const {testsList} = state;
-    return {
-        testsList
-    };
+  const {testsList} = state;
+  return {
+    testsList
+  };
 }
 
 export default connect(mapStateToProps)(TestList);
--- src/redux/reducers/tests.js
+++ src/redux/reducers/tests.js
@@ -1,42 +1,43 @@
 import {singleTestActionsTypes, testsActionsTypes} from "../constants";
 
 export function testsList(state = [], action) {
-
-    switch (action.type) {
-        case testsActionsTypes.TESTS_LIST_REQUEST:
-            return {
-                list: []
-            };
-        case testsActionsTypes.TESTS_LIST_SUCCESS:
-            return {
-                list: action.payload
-            };
-        case testsActionsTypes.TESTS_LIST_FAILED:
-            return {};
-        case testsActionsTypes.TESTS_LIST_MODIFY:
-            return {
-                list: state.list.list.filter(test => action.condition(test))
-            };
-        default:
-            return state;
-    }
+switch (action.type) {
+    case testsActionsTypes.TESTS_LIST_REQUEST:
+      return {
+        list: []
+      };
+    case testsActionsTypes.TESTS_LIST_SUCCESS:
+      return {
+        list: action.payload,
+        filteredList: action.payload
+      };
+    case testsActionsTypes.TESTS_LIST_FAILED:
+      return {};
+    case testsActionsTypes.TESTS_LIST_MODIFY:
+      return {
+        list: action.payload.initialList,
+        filteredList: action.payload.initialList.filter(test => action.payload.condition(test))
+      };
+    default:
+      return state;
+  }
 }
 
 export function singleTest(state = [], action) {
 
-    switch (action.type) {
-        case singleTestActionsTypes.TESTS_LIST_REQUEST:
-            return {
-                info: action.payload
-            };
-        case singleTestActionsTypes.TESTS_LIST_SUCCESS:
-            return {
-                info: action.payload
-            };
-        case singleTestActionsTypes.TESTS_LIST_FAILED:
-            return {};
-        default:
-            return state;
-    }
+  switch (action.type) {
+    case singleTestActionsTypes.TESTS_LIST_REQUEST:
+      return {
+        info: action.payload
+      };
+    case singleTestActionsTypes.TESTS_LIST_SUCCESS:
+      return {
+        info: action.payload
+      };
+    case singleTestActionsTypes.TESTS_LIST_FAILED:
+      return {};
+    default:
+      return state;
+  }
 
 }
